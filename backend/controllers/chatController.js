d7fa22edd466b20fcba74e4c36bb18e944108cbb
Hirruá Souza da Silva <96480452+Hirrua@users.noreply.github.com>
2025-04-22 14:07:27 -0300

    feat: analyze resume image with OCR Tesseract (#1)

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -2,8 +2,29 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import 'dotenv/config';
 import pdf from 'pdf-parse/lib/pdf-parse.js';
 import 'dotenv/config';
+import { createWorker } from 'tesseract.js';
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+export const extractTextFromImage = async (imageBuffer) => {
+  const worker = await createWorker();
+
+  const config = {
+    lang: 'por',
+    oem: 1,  
+    psm: 3       
+  };
+
+  try {
+    const { data: { text } } = await worker.recognize(imageBuffer, config);
+    console.log("Texto extraído da imagem:", text);
+    return text;
+
+  } catch (error) {
+    console.error("Erro ao extrair texto da imagem:", error);
+    throw new Error("Erro ao processar a imagem");
+  }
+};
 
 export const chatWithGemini = async (req, res) => {
   try {
@@ -11,10 +32,20 @@ export const chatWithGemini = async (req, res) => {
           return res.status(400).json({ error: "Envie a descrição da vaga e o currículo em PDF" });
       }
 
+      const file = req.files.curriculo[0];
       const pdfBuffer = req.files.curriculo[0].buffer;
-      const pdfData = await pdf(pdfBuffer);
-      const textoCurriculo = pdfData.text;
+      let textoCurriculo = '';
 
+      if (file.mimetype === 'application/pdf') {
+        const pdfData = await pdf(pdfBuffer);
+        textoCurriculo =  pdfData.text;
+      } else if (file.mimetype.startsWith('image/')) {
+        textoCurriculo = await extractTextFromImage(file.buffer);
+      } else {
+        return res.status(400).json({ error: "Formato não suportado. Use PDF ou imagens (JPG/PNG)" });
+      }
+      
+      
       const prompt = `Analise esta descrição de vaga e currículo e retorne um JSON estruturado com os seguintes campos:
       
       **DESCRIÇÃO DA VAGA:**
